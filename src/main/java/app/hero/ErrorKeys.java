04c98aba234c6fe74ef70f36b4c545c03127a144
Marek <pocztylion91@gmail.com>
2017-07-23 14:45:43 +0100

    changed errors and max/min interegers' location

--- src/main/java/app/hero/ErrorKeys.java
+++ src/main/java/app/hero/ErrorKeys.java
@@ -1,11 +1,8 @@
 package app.hero;
 
 public interface ErrorKeys {
-    String EMPTY_NAME = "Name can not be empty.";
-    String STATS_TOO_BIG = "Sum of stats can not be bigger than 10.";
-    String STAT_TOO_BIG = "A statistic can not be bigger than 5.";
-    String STAT_TOO_SMALL = "A statistic can not be a negative value.";
-    int MAX_STAT = 5;
-    int MIN_STAT = 0;
-    int MAX_STATS_SUM = 10;
+    String EMPTY_NAME = "EMPTY_NAME";
+    String STATS_SUM_TOO_BIG = "STATS_SUM_TOO_BIG";
+    String STAT_TOO_BIG = "STAT_TOO_BIG";
+    String STAT_TOO_SMALL = "STAT_TOO_SMALL";
 }
--- src/main/java/app/hero/HeroValidator.java
+++ src/main/java/app/hero/HeroValidator.java
@@ -3,11 +3,17 @@ package app.hero;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
+
 import static app.hero.ErrorKeys.*;
+
 import java.util.Arrays;
 
 @Component
 public class HeroValidator implements org.springframework.validation.Validator {
+
+    private static final int MAX_STAT = 5;
+    private static final int MIN_STAT = 0;
+    private static final int MAX_STATS_SUM = 10;
 
     public boolean supports(Class clazz) {
         return Hero.class.equals(clazz);
@@ -19,7 +25,7 @@ public class HeroValidator implements org.springframework.validation.Validator {
         Hero hero = (Hero) obj;
         int[] stats = {hero.getAgility(), hero.getIntelligence(), hero.getStrength()};
         if (Arrays.stream(stats).sum() > MAX_STATS_SUM) {
-            error.reject(STATS_TOO_BIG);
+            error.reject(STATS_SUM_TOO_BIG);
         }
         for (int stat : stats) {
             if (stat > MAX_STAT) {
